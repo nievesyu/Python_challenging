1f9cde797bd4ca2dceddc3ef5fc003a27aebe220
Yuting Chien <yutingchien@Yutings-MacBook-Pro.local>
2021-05-09 04:21:52 -0500

    finishupdate the result of analysis

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,63 @@
 import os
 import csv
 
-Election_csv = os.path.join("Resources", "election_data.csv")
+pollanalysis = os.path.join("Resources", "election_data.csv")
 
-# Open and read csv
-with open(Election_csv) as csv_file:
+with open(pollanalysis) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
-    # Read the header row first (skip this part if there is no header)
-    csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
+    csv_header = next(csv_reader)
+
+
+    Total_Votes = []
+    Candidate_List = []
+    Vote_Count = []
+    Percentage = []
+    Percent_Final = []
+   
+
+
+    for row in csv_reader:
+        Total_Votes.append(row[2])
+
+        DupCan = False
+        for candidate in Candidate_List:
+            if candidate == row[2]:
+                DupCan = True
+       
+        if DupCan == False:
+            Candidate_List.append(row[2])
+
+    for person in Candidate_List:
+        Vote_Count.append(Total_Votes.count(person))
+   
+    for count in Vote_Count:
+        Percentage.append(count/int(len(Total_Votes))*100)
+
+    for item in Percentage:
+        Percent_Final.append(round(item, 2))
+
+    results = dict()
+    Final_Results = []
+
+    for i in range(len(Candidate_List)):
+        results = {f"{Candidate_List[i]}: {Percent_Final[i]}% ({Vote_Count[i]})"}
+        Final_Results.append(results)
+
+    MaxVote = Vote_Count[0]
+    Winner = Candidate_List[0]
+
+    for j in range(len(Vote_Count)):
+        if Vote_Count[j] > MaxVote:
+            MaxVote = Vote_Count[j]
+            Winner = Candidate_List[j]
+
+   
+    print("Election Results")
+    print("---------------------------------------")
+    print(f"Total Votes: {len(Total_Votes)}")
+    print("---------------------------------------")
+    print(Final_Results)
+    print("---------------------------------------")
+    print(f"Winner: {Winner}")
+    print("---------------------------------------")
