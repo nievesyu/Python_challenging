f8f0ec2f6d739af32aecd0b30048150c875a1ace
Yuting Chien <yutingchien@Yutings-MacBook-Pro.local>
2021-05-09 01:48:33 -0500

    modify the main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,50 @@
 import os
 import csv
 
-Data_csv = os.path.join("Resources", "budget_data.csv")
+bankanalysis = os.path.join("Resources", "budget_data.csv")
 
-# Open and read csv
-with open(Data_csv) as csv_file:
+with open(bankanalysis) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
-    # Read the header row first 
-    csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
+    csv_header = next(csv_reader)
+
+    print("Bank Financial Analysis")
+    print("------------------------------------")
+
+
+    RowNum = 0
+    NetTol = []
+    MonthList = []
+   
+    for row in csv_reader:
+        RowNum = RowNum + 1
+   
+
+        Profit = int(row[1])
+        NetTol.append(Profit)
+        Month = row[0]
+        MonthList.append(Month)
+       
+    subtractlist = [NetTol[i+1] - NetTol[i] for i in range(len(NetTol) - 1)]
+    average = sum(subtractlist) / len(subtractlist)
+
+
+    greatestincrease = subtractlist[0]
+    greatestdecrease = subtractlist[0]
+
+    for j in range(len(subtractlist)):
+        if subtractlist[j] > greatestincrease:
+            greatestincrease = subtractlist[j]
+            increasemonth = MonthList[j+1]
+        if subtractlist[j] < greatestdecrease:
+            greatestdecrease = subtractlist[j]
+            decreasemonth = MonthList[j+1]
+
+   
+   
+    print(f"Total months: {RowNum}")
+    print(f"Net Total: {sum(NetTol)}")
+    print(f"Average Change: {average}")
+    print(f"Greatest Increase in Profit: {increasemonth} (${greatestincrease})")
+    print(f"Greatest Decrease in Profit: {decreasemonth} (${greatestdecrease})")
+       
